6e5e2962fe570c4cbea59eacf2e4578fe0d3d963
Scott Hutson <srhutson729@gmail.com>
2020-06-01 23:20:47 -0500

    Further training, started Season App

--- components/src/index.js
+++ components/src/index.js
@@ -40,3 +40,6 @@ const App = () => {
     );
 };
 ReactDOM.render(<App />, document.querySelector ('#root'))
+
+
+
---
+++ seasons/src/SeasonDisplay.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const SeasonDisplay = () => {
+    return <div>Season Display</div>;
+
+};
+
+export default SeasonDisplay;
