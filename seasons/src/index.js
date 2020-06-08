a0120a5665e6acc4b6071bf569b73ae5020b4b04
Scott Hutson <srhutson729@gmail.com>
2020-06-08 14:35:25 -0500

    updated tutorial work and added Sites and Stuff

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -1,33 +1,37 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import SeasonDisplay from './SeasonDisplay';
 
 class App extends React.Component {
-    constructor(props) {
-        super(props);
-
-        this.state={lat: null };
-        
+state = { lat: null, errorMessage: ''};
+ 
+    componentDidMount()  {
         window.navigator.geolocation.getCurrentPosition(
-            position => {
-                this.setState ({ lat: position.coords.latitude})
-            },
-            err => {
-                this.setState ({errorMessage: err.message})
-            }
-            );
-
+          position => this.setState({ lat: position.coords.latitude }),
+            err => this.setState({ errorMessage: err.message })
+      );
     }
-    render() {
-         return (
-              <div>
-                Latitude: {this.state.lat}
-                Error: {this.state.errorMessage}
-                </div>
 
-       }
+    render() {
+     if (this.state.errorMessage && !this.state.lat) {
+         return <div>Error: {this.state.errorMessage}</div>;
+        }
+ 
+     if (!this.state.errorMessage && this.state.lat) {
+         return <SeasonDisplay  lat={this.state.lat} />
+     }
+    
+      return <div>Loading!</div>; 
+      }
 }
 
-ReactDOM.render(
-    <App />,
-    document.querySelector('#root')
-    );
+
+ ReactDOM.render(<App />, document.querySelector('#root')) ;
+
+
+
+
+
+
+
+    
