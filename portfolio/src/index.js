fb377870d60570c867263b7111b832ddc41bd632
Scott Hutson <srhutson729@gmail.com>
2020-06-11 08:51:50 -0500

    Portfolio

---
+++ portfolio/src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const App =() => {
+    return <div>Scott Hutson</div>;
+
+};
+
+
+
+ReactDOM.render(
+    <App />,
+    document.querySelector('#root'));
