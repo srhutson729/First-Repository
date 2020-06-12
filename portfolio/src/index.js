5cb973ec06c308dd0a09398b8d42f559567adde5
Scott Hutson <srhutson729@gmail.com>
2020-06-11 22:50:35 -0500

    portfolio work

--- portfolio/src/index.js
+++ portfolio/src/index.js
@@ -1,10 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Button from 'react-bootstrap/Button';
 
 const App =() => {
-    return <div>Scott Hutson</div>;
+    return <div class='container-fluid'>
+        <h1 class='text-center'>Scott Hutson</h1>
+        <ul class="nav nav-pills nav-fill">
+  <li class="nav-item">
+    <a class="nav-link" href="#!">About Me</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" href="#!">Projects</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" href="#!">Resume</a>
+  </li>
+</ul>
+        <Button variant="danger">DANGER</Button>
+
+    </div>;
 
 };
+
 
 
 
