814b3ad8f8d8d7dd4e4494289d156256da69af92
Scott Hutson <srhutson729@gmail.com>
2021-01-28 23:21:02 -0600

    Added small bio and graphic

--- jcschmid/src/components/HeroSection.js
+++ jcschmid/src/components/HeroSection.js
@@ -1,15 +1,18 @@
 import React from 'react';
 import '../App.css';
-import { Button } from './Button';
 import './HeroSection.css';
-import JSHeadshot2 from "./JSHeadshot2.jpg";
+import Headshotweb from "../images/Headshotwebblack.jpg"
+
+
 
 function HeroSection() {
   return (
-    <div className='hero-container'>
-      <img src={JSHeadshot2} width={400} height={350} mode='fit' class='headshot'/>
-      <p>Welcome! My name is Joe</p>
-    </div> 
+    <div className='hero-container'> 
+<div class="jumbotron jumbotron-fluid">
+  <div class="container">
+    <p>Welcome! </p>
+  </div>
+</div> </div>
   );
 }
 
